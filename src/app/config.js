d266919930575e50ee226a43a690da4cbb9c78f6
Leonid Doroshenko <doroshenko@gmail.com>
2023-11-28 19:06:42 +0000

    App config file

---
+++ src/app/config.js
@@ -0,0 +1,10 @@
+/**
+ * Maximal number of data points stored and used in visualisation.
+ * @type {number}
+ */
+export const DATAPOINTS_AMOUNT = 100;
+/**
+ * Specifies timeout duration (in milliseconds) between weather data requests.
+ * @type {number}
+ */
+export const WEATHER_DATA_REQUEST_TIMEOUT = 10000;

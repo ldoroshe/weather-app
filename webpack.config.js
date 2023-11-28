23b48d0add0c0867617809be79ba1f41d2fb7d58
Leonid Doroshenko <doroshenko@gmail.com>
2023-11-28 16:51:32 +0000

    Environment: added webpack and .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/app/index.js",
+  },
+  devServer: {
+    port: 8081,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
